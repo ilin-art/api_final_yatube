5eaf05b7f14d716dd0be65995d51caeabef7745a
Ильин Артём <joker9099@mail.ru>
2022-03-28 22:26:50 +0300

    21 passed

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,24 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from django.urls import include, path
+
+from .views import GroupViewSet, PostViewSet, CommentViewSet
+
+
+router = SimpleRouter()
+
+router.register('groups', GroupViewSet) # Убрать basename если нет def get_queryset во вью
+router.register('posts', PostViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router.register('follow', PostViewSet)
+
 
 urlpatterns = [
-]
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+] 
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,28 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets 
+
+from posts.models import Group, Post, Comment
+from .serializers import GroupSerializer, PostSerializer, CommentSerializer
+from rest_framework import permissions
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    def get_queryset(self):
+        # Получаем id котика из эндпоинта
+        post_id = self.kwargs.get("post_id")
+        # И отбираем только нужные комментарии
+        new_queryset = Comment.objects.filter(post=post_id)
+        return new_queryset
